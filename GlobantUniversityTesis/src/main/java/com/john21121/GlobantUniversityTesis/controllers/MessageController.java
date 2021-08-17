c8e2331f1de45cca35433b789fe3b589332abe0a
Juan Abreu <juan.abreu@globant.com>
2021-08-17 12:13:58 -0300

    Planning Message Controller in order to further develop

--- GlobantUniversityTesis/src/main/java/com/john21121/GlobantUniversityTesis/controllers/MessageController.java
+++ GlobantUniversityTesis/src/main/java/com/john21121/GlobantUniversityTesis/controllers/MessageController.java
@@ -1,4 +1,29 @@
 package com.john21121.GlobantUniversityTesis.controllers;
 
+import com.john21121.GlobantUniversityTesis.mailingsystem.User;
+import com.john21121.GlobantUniversityTesis.repository.MessageRepository;
+import com.john21121.GlobantUniversityTesis.services.MessageService;
+import com.john21121.GlobantUniversityTesis.services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
 public class MessageController {
+
+    private final MessageRepository messageRepository;
+    private final MessageService messageService;
+    private final User user;
+    private final UserService userService;
+
+    public MessageController(MessageRepository messageRepository, MessageService messageService,
+                             User user, UserService userService) {
+        this.messageRepository = messageRepository;
+        this.messageService = messageService;
+        this.user = user;
+        this.userService = userService;
+    }
+
+
+
 }
