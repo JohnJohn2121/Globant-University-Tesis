e60e5d394f0946484885ccc807d4590a3378c950
Juan Abreu <juan.abreu@globant.com>
2021-08-20 04:05:26 -0300

    Added User Login in UserController

--- GlobantUniversityTesis/src/main/java/com/john21121/GlobantUniversityTesis/controllers/UserController.java
+++ GlobantUniversityTesis/src/main/java/com/john21121/GlobantUniversityTesis/controllers/UserController.java
@@ -3,9 +3,19 @@ package com.john21121.GlobantUniversityTesis.controllers;
 
 import com.john21121.GlobantUniversityTesis.exceptions.NotFoundException;
 import com.john21121.GlobantUniversityTesis.repository.UserRepository;
+import com.john21121.GlobantUniversityTesis.security.AuthRequest;
+import com.john21121.GlobantUniversityTesis.security.AuthResponse;
+import com.john21121.GlobantUniversityTesis.security.JwtUtil;
+import com.john21121.GlobantUniversityTesis.security.MyUserDetailsService;
 import com.john21121.GlobantUniversityTesis.services.UserService;
 import com.john21121.GlobantUniversityTesis.mailingsystem.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,6 +28,15 @@ public class UserController {
 
     //TODO Update the whole class
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
     private final UserService userService;
     private final UserRepository userRepository;
 
@@ -27,6 +46,26 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @RequestMapping(value="/login/",method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }
+        catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthResponse(jwt));
+    }
 
     @GetMapping("/finduser/{userid}")
     @ResponseStatus(HttpStatus.OK)
@@ -47,7 +86,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public Optional<User> updateUser(@PathVariable("userid")Long userId, @RequestBody User user){
         Optional<User> user1 = userRepository.findById(userId);
-        if (!user1.isPresent()){
+        if (user1.isEmpty()){
             throw new NotFoundException("This User does´nt exist");
         }
         return null;//userService.updateUserById(userId,user);
@@ -57,7 +96,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteUserById(@PathVariable("userid")Long userId){
         Optional<User> user1 = userRepository.findById(userId);
-        if (!user1.isPresent()){
+        if (user1.isEmpty()){
             throw new NotFoundException("This User does´nt exist");
         }
 
